0bf53da2b56a181ac3efc1d8c9ecc51849f7f1ab
PaMEllAaaa <6731338121@student.chula.ac.th>
2026-09-05 09:16:35 +0700

    chore: import shared UI components from feat/frontend/shared-components

---
+++ frontend/src/components/ui/Button.tsx
@@ -0,0 +1,27 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  icon?: ReactNode;
+  variant?: "dark" | "light" | "accent-500";
+}
+
+export function Button({
+  children,
+  icon,
+  className = "",
+  variant = "light",
+  ...props
+}: ButtonProps) {
+  const variantClass = {
+    light: "rounded-lg bg-white border px-5 py-1 text-small text-primary-500",
+    dark: "rounded-lg bg-primary-500 px-5 py-1 text-small text-white",
+    "accent-500": "rounded-lg bg-accent-500 px-5 py-1 text-small text-white",
+  };
+  return (
+    <button {...props} className={`${variantClass[variant]} ${className}`}>
+      {icon && <span>{icon}</span>}
+      {children}
+    </button>
+  );
+}
