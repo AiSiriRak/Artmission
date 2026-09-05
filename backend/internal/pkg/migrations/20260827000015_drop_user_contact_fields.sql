772755d70a5b2f1a6f9d02db0fcdce0c06edf5f2
DeepAung <i.deepaung@gmail.com>
2026-09-05 23:35:54 +0700

    refactor(backend): consolidate user migration baseline

--- backend/internal/pkg/migrations/20260827000015_drop_user_contact_fields.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-ALTER TABLE users DROP COLUMN first_name;
-ALTER TABLE users DROP COLUMN last_name;
-ALTER TABLE users DROP COLUMN phone_number;
-
--- +goose Down
-ALTER TABLE users ADD COLUMN first_name text NOT NULL DEFAULT '';
-ALTER TABLE users ADD COLUMN last_name text NOT NULL DEFAULT '';
-ALTER TABLE users ADD COLUMN phone_number text NOT NULL DEFAULT '';
