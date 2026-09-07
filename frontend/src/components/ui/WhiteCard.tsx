53cc518dece886a7db0497e14d3620a00a5bfe4b
AiSiriRak <siripudsorn@gmail.com>
2026-09-07 15:18:01 +0700

    chore(frontend): add options for customizing the card

--- frontend/src/components/ui/WhiteCard.tsx
+++ frontend/src/components/ui/WhiteCard.tsx
@@ -1,10 +1,20 @@
 interface WhiteCard {
   children: React.ReactNode;
+  margin?: string;
+  padding?: string;
+  roundsize?: string;
 }
 
-export function WhiteCard({ children }: WhiteCard) {
+export function WhiteCard({
+  children,
+  roundsize = "rounded-lg",
+  margin = "m-10",
+  padding = "p-16",
+}: WhiteCard) {
   return (
-    <div className="m-10 w-full max-w-md rounded-lg bg-white p-16 shadow-card">
+    <div
+      className={`${margin} ${roundsize} ${padding} w-full max-w-md bg-white shadow-card`}
+    >
       {children}
     </div>
   );
