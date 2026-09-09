cfbdb60087ed5f8249303e050fd0b706b910f248
AiSiriRak <siripudsorn@gmail.com>
2026-09-09 16:34:14 +0700

    feat(frontend): add header for customer role

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,10 @@
+export type PageType =
+  | "Home"
+  | "Login"
+  | "Register"
+  | "Notification"
+  | "Setting"
+  | "Artist Profile"
+  | "Order History";
+
+export type UserType = "customer" | "artist" | "admin";
